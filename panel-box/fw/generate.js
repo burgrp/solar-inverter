abaaf68f2128f03219ce0612c53c55ffb36032d4
Pavel Burgr <pb@drake.cz>
2022-03-29 00:28:57 +0200

    WIP

--- panel-box/fw/generate.js
+++ panel-box/fw/generate.js
@@ -5,7 +5,7 @@ const acTimerFreqHz = 50;
 const acTimerPrescaler = 256;
 
 const pwmTimerSteps = Math.floor(clkFreqHz / pwmTimerPrescaler / pwmTimerFreqHz);
-const acTimerSteps = Math.floor(clkFreqHz / acTimerPrescaler / acTimerFreqHz / 4) * 4;
+const acTimerSteps = Math.floor(clkFreqHz / acTimerPrescaler / acTimerFreqHz / 2) * 2;
 
 console.info(`namespace generated {
 
@@ -17,13 +17,13 @@ const target::tc::COUNT16::CTRLA::PRESCALER AC_TIMER_PRESCALER = target::tc::COU
 
 // values to be multiplied by level <0..100%> represented as <0..256> and shifted right 24 bits
 __attribute__((section(".text")))
-const int QUARTER_AC_CC[AC_TIMER_STEPS / 4] = {`);
-for (let i = 0; i < acTimerSteps / 4; i++) {
-    let pos = i / (acTimerSteps / 2 - 2);
+const int HALF_AC_CC[AC_TIMER_STEPS / 2] = {`);
+for (let i = 0; i < acTimerSteps / 2; i++) {
+    let pos = i / (acTimerSteps / 2 );
     let sin = Math.sin(Math.PI * pos);
     let val = Math.round(0x10000 * sin * pwmTimerSteps);
-    let last = i < acTimerSteps / 4 - 1;
-    console.info(`${val}${last && "," || ""}`);
+    let last = i < acTimerSteps / 2 - 1;
+    console.info(`${val}${last && "," || ""} // ${pos}: ${sin}`);
         
 }
 
